a8ce438a71f2da7325fb3c6bf5f659e72bcd93de
madewild <max.de.wilde@ulb.be>
2023-02-14 14:36:53 +0100

    enable user config

---
+++ user-config.py
@@ -0,0 +1,10 @@
+user_families_paths = ['./config']
+mylang = "wikidata"
+family = "wikidata"
+usernames['my']['my'] = u'Admin@semicbot'
+password_file = "user-password.py"
+minthrottle = 0
+maxthrottle = 0
+max_retries = 100
+
+
