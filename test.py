009af3ff6d48a5c52d1080fef2f3c3201727ca56
madewild <max.de.wilde@ulb.be>
2023-02-20 15:19:01 +0100

    better test

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-# Testing if bot login works properly
+# Testing if we can read from Wikibase
 
 import pywikibot
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 wikibase = pywikibot.Site("my", "my")
 wikibase_repo = wikibase.data_repository()
-wikibase_repo.login()
+#wikibase_repo.login()
 
 item = pywikibot.ItemPage(wikibase_repo, "Q1")
 item.get()
-print(item.labels['en'])
+print(item.labels['en']) # should print "Semantic Interoperability Community"
