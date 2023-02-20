c9ac2b8b58a5c397718ce050681153cf31298dcc
madewild <max.de.wilde@ulb.be>
2023-02-20 14:52:26 +0100

    add docstring

--- user-config.py
+++ user-config.py
@@ -1,10 +1,10 @@
+# Basic configuration to access the Wikibase
+
 user_families_paths = ['./config']
 mylang = "wikidata"
 family = "wikidata"
-usernames['my']['my'] = u'Admin@semicbot'
+usernames['my']['my'] = u'Admin'
 password_file = "user-password.py"
 minthrottle = 0
 maxthrottle = 0
 max_retries = 100
-
-
