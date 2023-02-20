71eeafa9eb3aa5562264fe0e44568a9d019a96d3
madewild <max.de.wilde@ulb.be>
2023-02-20 16:33:05 +0100

    add new label

--- test.py
+++ test.py
@@ -5,8 +5,12 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 wikibase = pywikibot.Site("my", "my")
 wikibase_repo = wikibase.data_repository()
-#wikibase_repo.login()
+wikibase_repo.login()
 
 item = pywikibot.ItemPage(wikibase_repo, "Q1")
 item.get()
 print(item.labels['en']) # should print "Semantic Interoperability Community"
+
+fr_label = "Communauté d'interopérabilité sémantique"
+new_label_data = {'fr': fr_label}
+item.editLabels(new_label_data, summary=u'Added French label')
